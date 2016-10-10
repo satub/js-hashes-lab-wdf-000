e2bc606c035e92521daa2ad6d738523ce9d30c4f
satub <satu.barnhill@gmail.com>
2016-10-10 13:06:44 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(resepti, aines, tarvitaan){
+  resepti[aines] = tarvitaan;
+  return resepti;
+}
+
+function removeIngredient(resepti, aines){
+  delete resepti[aines];
+  return resepti;
+}
+
+function updateIngredient(resepti, aines, tarvitaan){
+  resepti[aines] = tarvitaan;
+  return resepti;
+}
+
+function readRecipe(resepti){
+  for (var aines in resepti){
+    console.log("this recipe calls for " + resepti[aines] + " of " + aines);
+  }
+}
